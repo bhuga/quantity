7463e94f58e4bd879d551b5b14b0b0bda692ed72
Ben Lavender <blavender@gmail.com>
2009-12-31 23:32:14 +0100

    Updates to information, and what 'all' is.

--- lib/quantity/all.rb
+++ lib/quantity/all.rb
@@ -1,3 +1,5 @@
 require 'quantity/systems/si'
+# this could just as well be 'imperial', had to pick something.
 require 'quantity/systems/us'
 require 'quantity/unit/information'
+require 'quantity/systems/other'
